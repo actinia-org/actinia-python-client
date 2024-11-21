e75a009ea9081f82789a935f5e32640085417c37
ninsbl <stefan.blumentrath@gmx.de>
2024-11-21 11:46:07 +0100

    use kwargs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -41,7 +41,9 @@ class TestActiniaUtils(object):
     def test_request_and_check(self):
         """Test request_and_check utils function."""
         url = f"{ACTINIA_BASEURL}api/{ACTINIA_VERSION}/version"
-        resp = request_and_check("GET", url, ACTINIA_AUTH, status_code=200)
+        resp = request_and_check(
+            "GET", url, status_code=(200), **{"auth": ACTINIA_AUTH}
+        )
         assert "version" in resp
 
     def test_request_and_check_wrong_url(self):
@@ -49,7 +51,7 @@ class TestActiniaUtils(object):
         url = f"{ACTINIA_BASEURL}api/{ACTINIA_VERSION}/version_fail"
         err_msg = "The requested URL was not found on the server."
         with pytest.raises(Exception) as excinfo:
-            request_and_check("GET", url, ACTINIA_AUTH, status_code=200)
+            request_and_check("GET", url, status_code=(200), **{"auth": ACTINIA_AUTH})
         assert err_msg in str(excinfo.value)
 
     def test_request_and_check_wrong_auth(self):
@@ -58,7 +60,7 @@ class TestActiniaUtils(object):
         err_msg = "Unauthorized Access"
         wrong_auth = ("actinia-gdi", "wrong_pw")
         with pytest.raises(Exception) as excinfo:
-            request_and_check("GET", url, wrong_auth, status_code=200)
+            request_and_check("GET", url, status_code=(200), **{"auth": wrong_auth})
         assert err_msg in str(excinfo.value)
 
     def test_set_job_names(self):
