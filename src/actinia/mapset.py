0c45554c700799c354bda8cf1932b196a7905bf2
ninsbl <stefan.blumentrath@gmx.de>
2025-01-02 10:19:39 +0100

    address test faliures

--- src/actinia/mapset.py
+++ src/actinia/mapset.py
@@ -648,7 +648,7 @@ class Mapset:
                 {
                     "title": title,
                     "description": description,
-                    "temporal_type": temporal_type,
+                    "temporaltype": temporal_type,
                 },
             ),
         }
