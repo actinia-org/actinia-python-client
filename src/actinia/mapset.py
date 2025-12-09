8baf3e1d7953568f23b64f7f3a838b14ee443a60
Markus Neteler <neteler@gmail.com>
2025-12-09 14:34:11 +0100

    SPDX: license declaration cleanup
    This PR fixes license declarations and adds missing declarations in file headers. It also adds the official license text file.
    
    Goal: acceptable result with `reuse lint`

--- src/actinia/mapset.py
+++ src/actinia/mapset.py
@@ -6,25 +6,14 @@ actinia-python-client is a python client for actinia - an open source REST
 API for scalable, distributed, high performance processing of geographical
 data that uses GRASS GIS for computational tasks.
 
-Copyright (c) 2022 mundialis GmbH & Co. KG
+SPDX-FileCopyrightText: (c) 2022 mundialis GmbH & Co. KG
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+SPDX-License-Identifier: GPL-3.0-or-later
 """
 
 from __future__ import annotations
 
-__license__ = "GPLv3"
+__license__ = "GPL-3.0-or-later"
 __author__ = "Anika Weinmann, Corey White and Stefan Blumentrath"
 __copyright__ = "Copyright 2022, mundialis GmbH & Co. KG"
 __maintainer__ = "Anika Weinmann"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#######
+# actinia-core - an open source REST API for scalable, distributed, high
+# performance processing of geographical data that uses GRASS GIS for
+# computational tasks. For details, see https://actinia.mundialis.de/
+#
+# SPDX-FileCopyrightText: (c) 2016-2025 mundialis GmbH & Co. KG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+#######
 """
     Dummy conftest.py for actinia.
 
