c5015b42d2ad870a2ab9940cc42a8b2ce2974ba3
Markus Neteler <neteler@mundialis.de>
2025-12-09 16:33:55 +0200

    SPDX: license declaration cleanup (#62)
    This PR fixes license declarations and adds missing declarations in file headers. It also adds the official license text file.
        
    Goal: acceptable result with `reuse lint`

--- src/actinia/resources/templating.py
+++ src/actinia/resources/templating.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2018-2022 mundialis GmbH & Co. KG
+SPDX-FileCopyrightText: (c) 2023-2025 mundialis GmbH & Co. KG
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+SPDX-License-Identifier: GPL-3.0-or-later
 
 Template loader file
 """
 
-__license__ = "GPLv3"
+__license__ = "GPL-3.0-or-later"
 __author__ = "Carmen Tawalika"
-__copyright__ = "Copyright 2018-2022 mundialis GmbH & Co. KG"
+__copyright__ = "Copyright 2023-2025 mundialis GmbH & Co. KG"
 __maintainer__ = "mundialis GmbH % Co. KG"
 
 
